2fb613babac28b969376a274cff6bfd4f52dc82c
Filipe Madruga <52218589+Wizarsy@users.noreply.github.com>
2022-11-22 09:53:19 -0300

    Update cycle_sort_Java.java

--- cycle_sort_Java.java
+++ cycle_sort_Java.java
@@ -1,32 +1,44 @@
 import java.util.Arrays;
-public class cycle_sort_Java {
-  static void CycleSort(int[] array){
+
+public class cycle_sort_Java  
+{
+  static void CycleSort(int[] array)
+  {
     int size = array.length;
-    for(int start = 0; start < size; start++){
+    for(int start = 0; start < size; start++)
+    {
       int item = array[start];
       int index = start;
-      for(int i = start +1; i < size; i++){
-        if(array[i] < item){
+      for(int i = start +1; i < size; i++)
+      {
+        if(array[i] < item)
+        {
           index++;
         }
       }
-      if(index == start){
+      if(index == start)
+      {
         continue;
       }
-      while(item == array[index]){
+      while(item == array[index])
+      {
         index++;
       }
       int temp = array[index];
       array[index] = item;
       item = temp;
-      while(index != start){
+      while(index != start)
+      {
         index = start;
-        for(int i = start +1; i < size; i++){
-          if(array[i] < item){
+        for(int i = start +1; i < size; i++)
+        {
+          if(array[i] < item)
+          {
             index++;
           }
         }
-        while(item == array[index]){
+        while(item == array[index])
+        {
           index++;
         }
         temp = array[index];
@@ -35,11 +47,12 @@ public class cycle_sort_Java {
       }
     }
   }
-  public static void main(String[] args) {
-    int[] array_test = {10, 3, 5, 7, 1, 9, 6, 0, 4, 2};
-    System.out.println(Arrays.toString(array_test));
-    System.out.println("\n");
+  
+  public static void main(String[] args) 
+  {
+    int[] array_test = {8, 3, 5, 7, 1, 9, 6, 0, 4, 2};
+    System.out.println("Entrada: " + Arrays.toString(array_test) + "\n");
     CycleSort(array_test);
-    System.out.println(Arrays.toString(array_test));
+    System.out.println("Saida: " + Arrays.toString(array_test));
   }
 }
