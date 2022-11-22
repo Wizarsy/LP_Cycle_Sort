790660be06e1edb8585e53f251dac854496591c1
Filipe Madruga <52218589+Wizarsy@users.noreply.github.com>
2022-11-22 09:54:40 -0300

    Update cycle_sort_PHP.php

--- cycle_sort_PHP.php
+++ cycle_sort_PHP.php
@@ -1,42 +1,55 @@
 <?php
-function swap(&$a, &$b){
+function swap(&$a, &$b)
+{
   $temp = $a;
   $a = $b;
   $b = $temp;
 }
 
-function printArray(array $array){
-  for($i = 0; $i < count($array); $i++){
+function printArray(array $array)
+{
+  for($i = 0; $i < count($array); $i++)
+  {
     echo $array[$i] . " ";
   }
   echo "\n\n";
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-function CycleSort(array &$array){
+function CycleSort(array &$array)
+{
   $size = count($array);
-  for($start = 0; $start < $size; $start++){
+  for($start = 0; $start < $size; $start++)
+  {
     $item = $array[$start];
     $index = $start;
-    for($i = $start + 1; $i < $size; $i++){
-      if($array[$i] < $item){
+    for($i = $start + 1; $i < $size; $i++)
+    {
+      if($array[$i] < $item)
+      {
         $index++;
       }
     }
-    if($index == $start){
+    if($index == $start)
+    {
       continue;
     }
-    while($item == $array[$index]){
+    while($item == $array[$index])
+    {
       $index++;
     }
     swap($array[$index], $item);
-    while($index != $start){
+    while($index != $start)
+    {
       $index = $start;
-      for($i = $start + 1; $i < $size; $i++){
-        if($array[$i] < $item){
+      for($i = $start + 1; $i < $size; $i++)
+      {
+        if($array[$i] < $item)
+        {
           $index++;
         }
       }
-      while($item == $array[$index]){
+      while($item == $array[$index])
+      {
         $index++;
       }
       swap($array[$index], $item);
@@ -44,10 +57,10 @@ function CycleSort(array &$array){
   }
 }
 
-$array_teste = array(10, 3, 5, 7, 1, 9, 6, 0, 4, 2);
+$array_teste = array(8, 3, 5, 7, 1, 9, 6, 0, 4, 2);
+echo "Entrada: ";
 printArray($array_teste);
-
 CycleSort($array_teste);
-
+echo "Saida: ";
 printArray($array_teste);
 ?>
