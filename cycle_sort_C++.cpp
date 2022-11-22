44163704ac37d66911dd65e85448dbb6f7d7c74f
Filipe Madruga <52218589+Wizarsy@users.noreply.github.com>
2022-11-22 09:51:35 -0300

    Update cycle_sort_C++.cpp

--- cycle_sort_C++.cpp
+++ cycle_sort_C++.cpp
@@ -2,37 +2,50 @@
 
 using namespace std;
 
-void printArray(int *array, int size){
-  for(int i = 0; i < size; i++){
+void printArray(int *array, int size)
+{
+  for(int i = 0; i < size; i++)
+  {
     cout << array[i] << " ";
   }
   cout << "\n\n";
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-void CycleSort(int *array, int size){
-  for(int start = 0; start < size; start++){
+
+void CycleSort(int *array, int size)
+{
+  for(int start = 0; start < size; start++)
+  {
     int item = array[start];
     int index = start;
-    for(int i = start + 1; i < size; i++){
-      if(array[i] < item){
+    for(int i = start + 1; i < size; i++)
+    {
+      if(array[i] < item)
+      {
         index++;
       }
     }
-    if(index == start){
+    if(index == start)
+    {
       continue;
     }
-    while(item == array[index]){
+    while(item == array[index])
+    {
       index++;
     }
     swap(array[index], item);
-    while(index != start){
+    while(index != start)
+    {
       index = start;
-      for(int i = start + 1; i < size; i++){
-        if(array[i] < item){
+      for(int i = start + 1; i < size; i++)
+      {
+        if(array[i] < item)
+        {
           index++;
         }
       }
-      while(item == array[index]){
+      while(item == array[index])
+      {
         index++;
       }
       swap(array[index], item);
@@ -40,14 +53,14 @@ void CycleSort(int *array, int size){
   }
 }
 
-
-int array_teste[10] = {10, 3, 5, 7, 1, 9, 6, 0, 4, 2};
+int array_teste[10] = {8, 3, 5, 7, 1, 9, 6, 0, 4, 2};
 int array_size = sizeof(array_teste) / sizeof(int);
 
-int main(){
+int main()
+{
+  cout << "Entrada: ";
   printArray(array_teste, array_size);
-
   CycleSort(array_teste, array_size);
-
+  cout << "Saida: ";
   printArray(array_teste, array_size);
 }
